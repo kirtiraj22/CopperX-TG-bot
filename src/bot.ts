db0b537968a52d4c368b02f2f4ef24ba43740f63
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2025-03-17 22:41:24 +0530

    feat: bot configs

--- src/bot.ts
+++ src/bot.ts
@@ -4,8 +4,12 @@ import { helpCommand, startCommand } from "./commands/start";
 
 dotenv.config();
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN as string);
 
 bot.start(startCommand);
 bot.help(helpCommand);
 bot.launch().then(() => console.log("CopperX BOT is running..."));
+
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
---
+++ src/commands/auth.ts
@@ -0,0 +1,6 @@
+import { Telegraf } from "telegraf";
+import config from "../config";
+
+export function setupAuthCommands(bot: Telegraf){
+    bot.command("login", async (ctx) => {})
+}
